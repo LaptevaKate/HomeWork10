0a168071c356baf72388d30aa8899402703d344d
LaptevaKate <ekaterina213@yandex.ru>
2022-03-14 22:01:52 +0300

    pulse heart beat code

--- HomeWork10/PulseViewController.swift
+++ HomeWork10/PulseViewController.swift
@@ -7,56 +7,87 @@
 
 import UIKit
 
-class PulseViewController: UIViewController {
-
-    var animationCounter = 0
+final class PulseViewController: UIViewController {
     
-    @IBOutlet weak var textField: UITextField!
+    private var animationCounter = 0
     
-    @IBOutlet weak var heartImageView: UIImageView!
+    @IBOutlet private weak var textField: UITextField!
     
-    @IBOutlet weak var imageHeight: NSLayoutConstraint!
+    @IBOutlet private weak var heartImageView: UIImageView!
     
-
+    @IBOutlet private weak var imageHeight: NSLayoutConstraint!
+    @IBOutlet weak var imageWidth: NSLayoutConstraint!
+    
+    
     
     
     //MARK: - ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
+        textField.keyboardType = .numberPad
+        textField.returnKeyType = .done
+        textField.placeholder = "Enter heart beats per minute"
+        addDoneButtonOnKeyboard()
+        textField.delegate = self
     }
     //MARK: - Methods
     
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool
-      {
-        let allowedCharacters = CharacterSet.decimalDigits
-        let characterSet = CharacterSet(charactersIn: string)
-        return allowedCharacters.isSuperset(of: characterSet)
-      }
-    
-    @objc private func timer() {
-        Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(heartBeat), userInfo: nil, repeats: true)
+    private func addDoneButtonOnKeyboard(){
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "START", style: .done, target: self, action: #selector(self.doneButtonAction))
+        
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        
+        textField.inputAccessoryView = doneToolbar
     }
     
+    @objc private func doneButtonAction(){
+        textField.resignFirstResponder()
+    }
     
-    @objc private func heartBeat() {
-        UIView.animate(withDuration: 1, delay: 0, options: [.curveEaseInOut, .allowUserInteraction], animations: {
+    private func heartBeat(duration: Double) {
+        UIView.animate(withDuration: 60 / duration, delay: 0, options: [.curveEaseInOut], animations: {
             self.imageHeight.constant += 40
+            self.imageWidth.constant += 40
             self.view.layoutIfNeeded()
         }, completion:  {_ in
             self.imageHeight.constant -= 40
+            self.imageWidth.constant -= 40
             self.view.layoutIfNeeded()
             self.animationCounter += 1
             if (self.animationCounter < 15) {
-                self.heartBeat()
-            } else { self.animationCounter = 0
+                self.heartBeat(duration: duration)
+            } else {
+                self.animationCounter = 0
+                self.textField.isUserInteractionEnabled = true
+                self.textField.text = ""
             }
         })
     }
+}
+
+//MARK: - UITextFieldDelegate
+extension PulseViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange,replacementString string: String) -> Bool {
+        
+        return true
+    }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
     
-    //MARK: - IBActions
-    
-    @IBAction func startHeartBeatButton(_ sender: UIButton) {
-        heartBeat()
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let text = textField.text, !text.isEmpty, text != "0"  {
+            guard let textToDouble = Double(text) else { return }
+            heartBeat(duration: textToDouble)
+            textField.isUserInteractionEnabled = false
+        }
     }
 }
